94720db40f6202273bffc797d2d77ec5ccfb8d60
Konstantin Yakushev <kojo@kojo.ru>
2013-05-18 22:19:13 +0400

    First compilable version with sample service

---
+++ src/AutonomousServiceBus/Library1.fs
@@ -0,0 +1,32 @@
+﻿namespace AutonomousServiceBus
+    
+    open System
+    open ServiceStack.ServiceHost
+    open ServiceStack.WebHost.Endpoints
+
+    module AutonomousServiceBus =
+        
+
+        type TestType = {mutable test : string}
+        type TestTypeService() =
+            interface IService<TestType> with
+                member this.Execute (req:TestType) = req :> Object
+
+        type AppHost =
+            inherit AppHostBase
+            new() = { inherit AppHostBase ("Test service",typeof<TestType>.Assembly) }
+            override this.Configure container = 
+                base.Routes
+                    .Add<TestType>("/service/{test}")
+                        |>ignore
+
+
+
+
+        type Global =
+            inherit System.Web.HttpApplication
+            new() = { }
+            member x.Application_Start() = 
+                let appHost = new AppHost()
+                appHost.Init()
+
