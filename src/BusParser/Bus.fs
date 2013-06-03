0a9ede054c3f4925ed5d5c78a67597feb85190ed
Konstantin Yakushev <kojo@kojo.ru>
2013-06-03 13:36:39 +0400

    First version of lexer/parser

---
+++ src/BusParser/Bus.fs
@@ -0,0 +1,46 @@
+﻿// Sql.fs
+module Bus
+
+type value =   
+    | Int of int  
+    | Float of float  
+    | String of string  
+ 
+type param = 
+    | Def of string * value
+    | And of param * param
+
+type obj = 
+    |Inline of string * string * option<param>
+    |Outline of string
+
+type event = obj
+type command = obj
+type transform = obj
+
+type transforms = 
+    | OneTransform of transform
+    | Transfroms of transforms * transforms
+
+type rule = option<string> * event * option<transforms> * command
+
+//type dir = Asc | Desc   
+//type op = Eq | Gt | Ge | Lt | Le    // =, >, >=, <, <=   
+// 
+//type order = string * dir   
+// 
+//type where =   
+//    | Cond of (value * op * value)   
+//    | And of where * where   
+//    | Or of where * where   
+// 
+//type joinType = Inner | Left | Right   
+// 
+//type join = string * joinType * where option    // table name, join, optional "on" clause   
+// 
+//type sqlStatement =   
+//    { Table : string;   
+//        Columns : string list;   
+//        Joins : join list;   
+//        Where : where option;   
+//        OrderBy : order list }
---
+++ src/BusParser/Program.fs
@@ -0,0 +1,26 @@
+﻿open System   
+open Bus   
+let x = "
+ event 'shootEvery2Seconds' : 'repeat'
+with period = 2000;
+transform 'elapsedToMessage' : 'rename'
+with renames = [('elapsedMs','message')];
+command 'callMyService' : 'callRest'
+with host = 'http://localhost:3883/store/message';
+protocol = 'JSON';
+rule 'rule1': when 'shootEvery2Seconds' transform 'elapsedToMessage' do 'callMyService'
+"   
+//    SELECT x, y, z   
+//    FROM t1   
+//    LEFT JOIN t2   
+//    INNER JOIN t3 ON t3.ID = t2.ID   
+//    WHERE x = 50 AND y = 20   
+//    ORDER BY x ASC, y DESC, z   
+//"   
+ 
+let lexbuf = Lexing.LexBuffer<_>.FromString x
+let y = BusParser.start BusLexer.tokenize lexbuf   
+printfn "%A" y   
+ 
+Console.WriteLine("(press any key)")   
+Console.ReadKey(true) |> ignore
